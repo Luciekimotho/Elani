bff4f7f4ee6a373353f9c6910613967236448726
Luciekimotho <luciekimotho@gmail.com>
2015-11-30 23:37:38 +0300

    Implementing butterknife library

--- app/src/main/java/com/lucie/myapplication/PushNotif.java
+++ app/src/main/java/com/lucie/myapplication/PushNotif.java
@@ -2,12 +2,36 @@ package com.lucie.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.InjectView;
+import butterknife.ButterKnife;
 
 public class PushNotif extends AppCompatActivity {
+//EditText badge, pin;
+    //Button login, request_pin;
+
+    @InjectView(R.id.badge_no)
+    EditText badge;
+
+    @InjectView(R.id.pin_no)
+    EditText pin;
+
+    @InjectView(R.id.login)
+    Button login;
+
+    @InjectView(R.id.request_pin)
+    Button request;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_push_notif);
+
+
+
     }
 }
